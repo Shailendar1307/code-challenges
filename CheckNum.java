d10a4a3c11de947e07068ffb2b285c52bbba6828
Shailendar1307 <31620587+Shailendar1307@users.noreply.github.com>
2017-09-04 17:22:48 +0530

    Create CheckNum.java

---
+++ CheckNum.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class CheckNum{
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+if(a>0)
+System.out.println("POSITIVE NUMBER");
+else if(a<0)
+System.out.println("NEGATIVE NUMBER");
+else 
+System.out.println("ZERO");
+}
+}
